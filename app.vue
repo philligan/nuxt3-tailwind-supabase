2e32f42e541315ff1191970510352c7b5112787f
Phil Gilligan <phil.gilligan@mandogroup.com>
2023-04-24 11:02:35 +0100

    Create cat image gallery

--- app.vue
+++ app.vue
@@ -1,5 +1,20 @@
+<script setup lang="ts">
+const client = useSupabaseClient();
+
+const { data: images } = await useAsyncData(
+  "images",
+  async () => client.from("images").select("*").order("created_at"),
+  {
+    transform: (result) => result.data,
+  }
+);
+
+// client.from("images").insert({ src: "test", title: "test", author: "test" });
+// client.from('images').delete().eq('title', 'Jennifur')
+</script>
+
 <template>
   <div>
-    <NuxtWelcome />
+    <CatImageGallery :images="images" />
   </div>
 </template>
---
+++ components/CatImage.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+const props = defineProps({
+  src: {
+    type: String,
+    required: true,
+  },
+  alt: {
+    type: String,
+    required: true,
+  },
+});
+</script>
+
+<template>
+  <div class="flex flex-wrap w-1/3">
+    <div class="w-full p-1 md:p-2">
+      <nuxt-img
+        :src="src"
+        :alt="alt"
+        class="block object-cover object-center w-full h-hull rounded-lg"
+        format="webp"
+      />
+    </div>
+  </div>
+</template>
